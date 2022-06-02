312d8cc1caacefa6cd02fe375cf50586f3770321
simone mwangi <mwanjiis4@gmail.com>
2022-06-02 22:03:35 +0300

    Commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from hidden_4 import *
-    arr = dir()
-    for i in range(0, len(arr)):
-        if arr[i][0:2] != "__":
-            print("{}".format(arr[i]))
+    import hidden_4 as hidden
+
+    for name in dir(hidden):
+        if not name.startswith("__"):
+            print(name)
